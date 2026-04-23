3ea895e57f0ea75a2880a5bf60042460fb1e2c13
Luiz Mota <118900569+LuizMottaX@users.noreply.github.com>
2026-04-22 20:43:53 -0400

    ajustando fundo global da pagina

--- av2-luiz-mota-frameworks/src/main.js
+++ av2-luiz-mota-frameworks/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+document.body.style.margin = '0'
+document.body.style.background = '#0a0a1a'
+document.body.style.minHeight = '100vh'
+
 createApp(App).mount('#app')
