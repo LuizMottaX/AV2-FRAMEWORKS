684b1892e9b2aca82748c071ec6364c37ab7b1c4
Luiz Mota <118900569+LuizMottaX@users.noreply.github.com>
2026-04-22 20:42:56 -0400

    adicionando listagem com v-for, remocao individual e contador de itens

--- av2-luiz-mota-frameworks/src/App.vue
+++ av2-luiz-mota-frameworks/src/App.vue
@@ -10,6 +10,10 @@ function adicionarProduto() {
   produtos.value.push({ id: Date.now(), nome })
   novoProduto.value = ''
 }
+
+function removerProduto(id) {
+  produtos.value = produtos.value.filter(p => p.id !== id)
+}
 </script>
 
 <template>
@@ -23,6 +27,21 @@ function adicionarProduto() {
         placeholder="Digite o nome do produto..."
       />
       <button @click="adicionarProduto">Adicionar</button>
+    </div>
+
+    <div v-if="produtos.length > 0" class="lista-area">
+      <h2>Minha Cesta de Compras</h2>
+      <p class="contador">{{ produtos.length }} {{ produtos.length === 1 ? 'item' : 'itens' }} na cesta</p>
+      <ul>
+        <li v-for="produto in produtos" :key="produto.id">
+          <span>{{ produto.nome }}</span>
+          <button class="btn-remover" @click="removerProduto(produto.id)">Remover</button>
+        </li>
+      </ul>
+    </div>
+
+    <div v-else class="vazio">
+      <p>Sua cesta está vazia! Adicione produtos para começar.</p>
     </div>
   </div>
 </template>
